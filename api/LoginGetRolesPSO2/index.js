72e183cfdda35650b22e6eddf971eedf9b326345
SliWhist <122509026+SliWhist@users.noreply.github.com>
2023-01-26 18:38:55 -0500

    role test

--- api/LoginGetRolesPSO2/index.js
+++ api/LoginGetRolesPSO2/index.js
@@ -1,19 +1,77 @@
+const sql = require('mssql');
+
+const path = require('path');
+
+require('dotenv').config()
+
+const config = {
+	user: process.env["DB_USER"], // better stored in an app setting such as process.env.DB_USER
+	password: process.env["DB_PASSWORD"], // better stored in an app setting such as process.env.DB_PASSWORD
+	server: process.env["DB_SERVER"], // better stored in an app setting such as process.env.DB_SERVER
+	database: process.env["DB_NAME"], // better stored in an app setting such as process.env.DB_NAME
+	authentication: {
+		type: 'default'
+	},
+	options: {
+		encrypt: true
+	}
+}
+
 module.exports = async function (context, req) {
 
-    var data = {
-        "roles": [
-          "Reader",
-          "Contributor"
-        ]
-      }
+	try {
+		var poolConnection = await sql.connect(config);
+        //console.log(req.body);
+        const userID = req.body;
+        //console.log(userID.userId);
+        //console.log("BLEP")
 
-    data = JSON.stringify(data);
-    data = JSON.parse(data);
+		var sqlQuery = `
 
-    context.res = {
-        contentType: "application/json",
-            status: 200, /* Defaults to 200 */
-            body: data
-    };
+        SELECT
+            ui.Role
+
+        FROM Users.Information as ui
+            INNER JOIN Players.Information AS pi
+            ON pi.PlayerID = ui.PlayerID
+            INNER JOIN Players.Customization AS pc
+            ON pc.PlayerID = ui.PlayerID
+        WHERE
+            ui.UserID = @UserID`;
+			
+		var results = await poolConnection.request().input('UserID',sql.NVarChar, userID.userId).query(sqlQuery);
+		
+		var returner = results.recordset;
+		//console.log(results);
+		poolConnection.close();
+
+        console.log(returner[0]);
+
+        var data = {
+          "roles": [
+            returner[0].Role
+          ]
+        }
+
+        data = JSON.stringify(data);
+        data = JSON.parse(data);
+
+        //data = JSON.stringify(data);
+        //data = JSON.parse(data);
+        console.log(data);
+        context.res = {
+            contentType: "application/json",
+                status: 200, /* Defaults to 200 */
+                body: data
+        };
+
+    }
+    catch (err) {
+		console.error(err.message);
+        var data = {
+            "version": "1.0.0",
+            "action": "Continue",
+        }
+	}
 
 }
