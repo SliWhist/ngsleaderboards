e1932082638c58b133918e52447c865efc5d3798
SliWhist <122509026+SliWhist@users.noreply.github.com>
2023-01-26 10:06:05 -0500

    claims test

--- api/LoginLoadPSO2/index.js
+++ api/LoginLoadPSO2/index.js
@@ -1,13 +1,18 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    var data = {
+        "version": "1.0.0",
+        "action": "Continue",
+        "extension_CustomAttribute": "value" // return claim
+    }
+
+    data = JSON.stringify(data);
+    data = JSON.parse(data);
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        contentType: "application/json",
+            status: 200, /* Defaults to 200 */
+            body: data
     };
+
 }
