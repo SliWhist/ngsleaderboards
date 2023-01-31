dcd8efb928e3ca100e6bec11b88a8078ca312ca8
SliWhist <122509026+SliWhist@users.noreply.github.com>
2023-01-31 18:32:42 -0500

    hail mary

--- api/CreateUserPSO2/index.js
+++ api/CreateUserPSO2/index.js
@@ -31,7 +31,7 @@ module.exports = async function (context, req) {
         INSERT INTO dbo.TestTable2(garbage)
         VALUES(@name)`;
 			
-		await poolConnection.request().input('name',sql.NVarChar, newUser.displayName).query(sqlQuery);
+		await poolConnection.request().input('name',sql.NVarChar, req.body).query(sqlQuery);
 
         /*sqlQuery = `
         
