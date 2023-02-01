b501014a9f2a720c8b2308d9b7b485312eef8c8b
SliWhist <122509026+SliWhist@users.noreply.github.com>
2023-01-31 21:36:48 -0500

    last

---
+++ files/setup/setup.js
@@ -0,0 +1,42 @@
+function finishSetup() {
+    disableButtons();
+    characterName = document.getElementById('characterNameInput').value;
+    //console.log(characterName);
+    serverID = document.querySelector('input[name="serverRadio"]:checked').value;
+    //console.log(serverID);
+    ConfirmSettings(characterName,serverID);
+}
+
+
+
+async function getUserInfo() {
+    const response = await fetch('/.auth/me');
+    const payload = await response.json();
+    const { clientPrincipal } = payload;
+    return clientPrincipal;
+}
+
+async function ConfirmSettings (characterName,serverID) {
+	
+    var userkey = await getUserInfo();
+
+	const httpRequest = new XMLHttpRequest();
+	
+	httpRequest.onreadystatechange = () => {
+		if(httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200) {
+			//console.log(httpRequest.response);
+            window.location.replace("/logout");
+		}
+	}
+
+    var param = characterName + '@!@!@' + serverID + '@!@!@' + userkey;
+	
+	httpRequest.open('POST', '/api/firstSettings', true);
+	httpRequest.setRequestHeader('Content-type', 'x-www-form-urlencoded');
+	httpRequest.send(param);
+}
+
+
+function disableButtons() {
+	document.getElementById('submitrun').classList.add("disabled");
+}
